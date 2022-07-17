fac43e6199236380640bfa793ff5ffee3f713ea7
zuhu2195 <zuhu2195@gmail.com>
2022-07-17 18:28:58 +0530

    Merge conflict test1 master 1

--- ml_model.py
+++ ml_model.py
@@ -11,4 +11,10 @@ def model2():
 def model3():
     print("Adding code for model 3")
 
+<<<<<<< HEAD
 print("print model 2 is performing the best")
+=======
+print("print model 3 is performing the best out of all")
+
+print("Added some difference here")
+>>>>>>> 92626d0 (Merge conflict test1 master)
