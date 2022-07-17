e3e336592698057ce9e564818db0b536051d1adb
zuhu2195 <zuhu2195@gmail.com>
2022-07-17 17:08:49 +0530

    added three new ml model code

---
+++ ml_model.py
@@ -0,0 +1,12 @@
+
+print("adding all ML related code here")
+
+
+def model1():
+    print("Adding code for model 1")
+
+def model2():
+    print("Adding code for model 2")
+
+def model3():
+    print("Adding code for model 3")
